4c2f02228367d3b389717a2c6d374e05fa800779
Lucas Ribeiro <lucasrgsilva99@gmail.com>
2025-09-24 20:17:01 -0300

    first commit

---
+++ Exercício_04/Calculadora_price.py
@@ -0,0 +1,12 @@
+nome_do_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+
+preco_total = preco_unitario * quantidade
+
+
+print("Produto:", nome_do_produto)
+print("Preço unitário:", preco_unitario)
+print("Quantidade:", quantidade)
+print("O preço total da compra é:", preco_total)
